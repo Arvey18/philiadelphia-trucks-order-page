ddc772999be1249ad3f71baaf751cebbfbdf8f01
Arvey Jimenez <arve505@gmail.com>
2025-08-04 02:57:10 +0800

    applied responsive.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const overlayBackground = document.getElementById("overlay-background");
+const sidebar = document.getElementById("sidebar");
+
+hamburgerMenu.addEventListener("click", () => {
+  overlayBackground.classList.toggle("active");
+  sidebar.classList.toggle("active");
+});
+
+overlayBackground.addEventListener("click", () => {
+  overlayBackground.classList.toggle("active");
+  sidebar.classList.toggle("active");
+});
